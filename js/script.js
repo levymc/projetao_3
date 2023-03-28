a621d1a22f724b6a5c48157393e582c6e7c2b6fc
levymc <levymcruz@gmail.com>
2023-03-28 20:35:26 -0300

    botão respondendo a seleção

--- js/script.js
+++ js/script.js
@@ -10,40 +10,75 @@ divs.forEach(div => {
 });
 
 function selecionarPost(event) {
-  if (contador1 == 1 && contador2 == 1 && contador3 == 1){
-    alert("Grupo já selecionado!")
-    }else{
-    // Remove a classe de seleção de todas as divs de posts
-    const posts = document.querySelectorAll('.post');
-      if (contador === 3){
-        posts.forEach(post => post.classList.remove('post-selecionada'));
-        contador = 0;
-        contador1 = 0;
-        contador2 = 0;
-        contador3 = 0;
-        }
-      // Adiciona a classe de seleção na div clicada
-      const divClicada = event.target.closest('.post');
-      var divPai = divClicada.parentNode;
-      var classeDivPai = divPai.classList[0];
-      if (classeDivPai === 'postagens1' && contador1 < 1){
-      contador1 += 1;
-      contador += 1;
-      divClicada.classList.add('post-selecionada');
-      console.log(divClicada.innerText);
-      }else if(classeDivPai === 'postagens2' && contador2 < 1){
-      contador2 += 1;
-      contador += 1;
-      divClicada.classList.add('post-selecionada');
-      console.log(divClicada.innerText);
-      }else if(classeDivPai === 'postagens3' && contador3 < 1){
-      contador3 += 1;
-      contador += 1;
-      divClicada.classList.add('post-selecionada');
-      console.log(divClicada.innerText);
-      }
+  // Remove a classe de seleção de todas as divs de posts
+  const posts = document.querySelectorAll('.post');
+  if (contador == 3) {
+    posts.forEach(post => post.classList.remove('post-selecionada'));
+    contador = 0;
+    contador1 = 0;
+    contador2 = 0;
+    contador3 = 0;
+  }
+
+  // Adiciona a classe de seleção na div clicada
+  const divClicada = event.target.closest('.post');
+  const divPai = divClicada.parentNode;
+  const classeDivPai = divPai.classList[0];
+  let itemJaSelecionado = false;
+
+  // Verifica se já há algum item selecionado na mesma categoria da div clicada
+  if (classeDivPai == 'postagens1') {
+    if (contador1 > 0) {
+      itemJaSelecionado = true;
+      const postagens1 = document.querySelectorAll('.postagens1 .post-selecionada');
+      postagens1.forEach(post => post.classList.remove('post-selecionada'));
+      contador -= contador1;
+      contador1 = 0;
+    }
+  } else if (classeDivPai == 'postagens2') {
+    if (contador2 > 0) {
+      itemJaSelecionado = true;
+      const postagens2 = document.querySelectorAll('.postagens2 .post-selecionada');
+      postagens2.forEach(post => post.classList.remove('post-selecionada'));
+      contador -= contador2;
+      contador2 = 0;
+    }
+  } else if (classeDivPai == 'postagens3') {
+    if (contador3 > 0) {
+      itemJaSelecionado = true;
+      const postagens3 = document.querySelectorAll('.postagens3 .post-selecionada');
+      postagens3.forEach(post => post.classList.remove('post-selecionada'));
+      contador -= contador3;
+      contador3 = 0;
     }
   }
+
+  // Adiciona a classe de seleção na div clicada, caso não haja item já selecionado na mesma categoria
+  if (!itemJaSelecionado) {
+    divClicada.classList.add('post-selecionada');
+    console.log(divClicada.innerText);
+    if (classeDivPai == 'postagens1') {
+      contador1 += 1;
+    } else if (classeDivPai == 'postagens2') {
+      contador2 += 1;
+    } else if (classeDivPai == 'postagens3') {
+      contador3 += 1;
+    }
+    contador += 1;
+  }
+
+  // Verifica se já foram selecionados 3 itens para ativar o botão
+  if (contador == 3) {
+    const orderBtn = document.querySelector('[data-test="order-btn"]');
+    orderBtn.removeAttribute('disabled');
+    orderBtn.style.backgroundColor = '#50D074';
+    orderBtn.textContent = 'Fechar pedido';
+    orderBtn.classList.add('cursor-pointer');
+  }
+}
+
+
+
 
 
 function adicionarPostagem() {
