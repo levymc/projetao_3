be1491e4077de857275a175cc458284f44cb46eb
levymc <levymcruz@gmail.com>
2023-03-28 19:28:31 -0300

    Divs estão sendo selecionadas

--- js/script.js
+++ js/script.js
@@ -1,50 +1,11 @@
-const btnAdicionarPostagem1 = document.getElementById("adicionar-post1");
-const btnAdicionarPostagem2 = document.getElementById("adicionar-post2");
-const btnAdicionarPostagem3 = document.getElementById("adicionar-post3");
-const postagens1 = document.querySelector(".postagens1");
-const postagens2 = document.querySelector(".postagens2");
-const postagens3 = document.querySelector(".postagens3");
-
-btnAdicionarPostagem1.addEventListener("click", () => {
-  const novaPostagem = document.createElement('div');
-  novaPostagem.classList.add('img-post');
-
-  const listaImagens = ['clara.png', 'monique1.png', 'ruivo.png', 'frogao.png', 'thais.png'];
-  const imagemAleatoria = listaImagens[Math.floor(Math.random() * listaImagens.length)];
-
-  novaPostagem.style.backgroundImage = `url(./static/img/${imagemAleatoria})`;
-
-  const row1 = document.querySelector('.postagem1');
-  row1.appendChild(novaPostagem);
-});
-// btnAdicionarPostagem1.addEventListener("click", () => {
-//   const novaPostagem = document.createElement("div");
-//   novaPostagem.classList.add("post");
-//   postagens1.appendChild(novaPostagem);
-// });
-btnAdicionarPostagem2.addEventListener("click", () => {
-  const novaPostagem = document.createElement("div");
-  novaPostagem.classList.add("post");
-  postagens2.appendChild(novaPostagem);
-});
-btnAdicionarPostagem3.addEventListener("click", () => {
-  const novaPostagem = document.createElement("div");
-  novaPostagem.classList.add("post");
-  postagens3.appendChild(novaPostagem);
+const divs = document.querySelectorAll('.post');
+divs.forEach(div => {
+  div.addEventListener('click', () => {
+    const conteudo = div.textContent.trim();
+    console.log(conteudo);
+  });
 });
 
-// function adicionarPostagem() {
-//   const novaPostagem = document.createElement('div');
-//   novaPostagem.classList.add('post');
-
-//   const listaImagens = ['clara.png', 'monique1.png', 'ruivo.png', 'frogao.png', 'thais.png'];
-//   const imagemAleatoria = listaImagens[Math.floor(Math.random() * listaImagens.length)];
-
-//   novaPostagem.style.backgroundImage = `url(./static/img/${imagemAleatoria})`;
-
-//   const row1 = document.querySelector('.postagens1');
-//   row1.appendChild(novaPostagem);
-// }
 
 function adicionarPostagem() {
   const novaPostagem = document.createElement('div');
